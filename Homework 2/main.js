cd660b6c920bc165dc50f3bb0007b3454feb30b7
dzmitrymukaseyeu <dimamukas@gmail.com>
2020-04-29 22:35:09 +0300

    added all 12 task

--- Homework 2/main.js
+++ Homework 2/main.js
@@ -1,35 +1,34 @@
 var myArr1 = ['dima','dasha','pasha'];
 
 //task 1
-
 myArr1.unshift('sasha');
-
 console.log(myArr1);
 
 
 // task 2
-
 myArr1.push('tasha');
-
 console.log(myArr1);
 
 
 // task 3 
-
 delete myArr1 [2];
-
 console.log(myArr1);
 
 
 myArr1.splice(0,3);
-
 console.log(myArr1);
 
 
+// task 4
+var a = [1, 2, 2, 4];
+var t = [];
 
-
-myArr1.push('pasha');
-var a = [1,2,2,4];
+for (var i = 0; i < a.length; i++) {
+    if (t.indexOf(a[i]) == -1) {
+        t.push(a[i]);
+    };
+};
+console.log(t);
 
 
 // task 5
@@ -103,10 +102,21 @@ for (var i = 0; i < myArr5.length; i++) {
 
 
 // task 9
+var arr = [{ title: 'fdfkdlfksdl' }, { title: 'gkdgkfggkl' }];
+var newArr = [];
 
 
 
+for (var i = 0; i < arr.length; i++) {
 
+    var value = arr[i];
+
+    value.titleUppercased = value.title.toUpperCase();
+    newArr.push(arr[i]);
+
+}
+
+console.log(newArr);
 
 
 // task 10
@@ -119,6 +129,41 @@ for (var i = 0; i < myArr6.length; i++) {
     }
     
     };
+
+
+//task 11 
+var arr11 = [1, 2, 3, 4, 5];
+
+for (var i = 0; i < arr11.length; i++) {
+    if (arr11[i] % 2 == 0) {
+        console.log('Это четное число- ' + arr11[i])
+    };
+    else {
+        console.log('Это нечетное число- ' + arr11[i]);
+    };
+};
+
+
+// task 12
+
+function myFunc1(num) {
+    return num * num;
+};
+
+
+function myFunc2(func) {
+    var arr12 = [];
+
+    arr12.push(func(8));
+
+    return arr12;
+}
+
+myFunc2(myFunc1);
+
+console.log(myFunc2(myFunc1));
+
+
 
 
 
