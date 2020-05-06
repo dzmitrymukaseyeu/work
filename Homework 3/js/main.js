c55a4bbf90e7a44fbaff9745874343c37240dc61
dzmitrymukaseyeu <dimamukas@gmail.com>
2020-05-06 12:13:30 +0300

    added 1 and 3 task

---
+++ Homework 3/js/main.js
@@ -0,0 +1,26 @@
+var bod = document.body;
+
+var btn = document.querySelector('._toggle');
+var clickHandler = function() {
+
+    bod.classList.toggle('_color-scheme-light');
+
+};
+
+btn.addEventListener('click', clickHandler);
+
+
+(function (num) {
+    var tx = document.getElementsByClassName('l-tasks__item-text')[0];
+    if (typeof num === 'number') {
+        if (num % 2 === 0) {
+            tx.innerText = `Number ${num} is even`;
+        }
+        else {
+            tx.innerText = `Number ${num} is odd`;
+        }
+    }
+    else {
+        tx.innerText = 'This is not a number';
+    }
+})(7);
