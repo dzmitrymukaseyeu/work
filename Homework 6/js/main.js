14e01016a6785255056a6e9df6e54bf721e8a604
dzmitrymukaseyeu <dimamukas@gmail.com>
2020-05-20 13:11:44 +0300

    added slider

--- Homework 6/js/main.js
+++ Homework 6/js/main.js
@@ -1,5 +1,5 @@
 var body = document.body;
-var color = [];
+var colors = [];
 var obj = {
     field1: 1,
     field2: 'summer',
@@ -7,20 +7,50 @@ var obj = {
     field4: ['#00bcd4', ' #ffc107', '#2196f3'],
     field5: false
 };
+var counter = 0;
 var btnColor = document.querySelector('button');
 
-btnColor.addEventListener('click',function () {
-    for(var prop in obj) {
-        if (obj[prop] instanceof Array) {
-            color = obj[prop];
-            console.log(color);
-        }
+btnColor.addEventListener('click', function () {
+    i = Math.floor(Math.random() * (colors.length));
+    counter = i;
+    if(!colors.length) {
+        for(var prop in obj) {
+            if (obj[prop] instanceof Array) {
+                colors = obj[prop];
+            };
+        };
+    };
+    body.style.backgroundColor = colors[i];
+
+});
+
+body.addEventListener('keydown', function (e) {
+    
+
+    if ((e.code === 'ArrowRight' && colors.length === 0) || (e.code === 'ArrowLeft' && colors.length === 0)) {
+        alert ('Загрузите массив');
     };
 
-    var i = Math.floor(Math.random() * (color.length + 1))
-    console.log(i);
-    console.log(color.length)
-   
+    if (e.code === 'ArrowRight' && counter < colors.length - 1){
+        body.style.backgroundColor = colors[++counter];
 
-    
-})
+    } else if (e.code === 'ArrowRight' && counter === colors.length - 1) {
+        counter = 0
+        body.style.backgroundColor = colors[counter];
+    };
+
+
+    if (e.code === 'ArrowLeft' && counter <= colors.length - 1 && counter > 0) {
+        body.style.backgroundColor = colors[--counter];
+
+    } else if (e.code === 'ArrowLeft' && counter <= 0 ) {
+        counter = colors.length - 1;
+        body.style.backgroundColor = colors[counter];
+    }
+
+});
+
+
+
+
+
