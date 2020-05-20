780b5d0effc463756be7565c79e1d01d49052921
dzmitrymukaseyeu <dimamukas@gmail.com>
2020-05-20 21:50:26 +0300

    fixed bugs

--- Homework 6/js/main.js
+++ Homework 6/js/main.js
@@ -11,8 +11,6 @@ var counter = 0;
 var btnColor = document.querySelector('button');
 
 btnColor.addEventListener('click', function () {
-    i = Math.floor(Math.random() * (colors.length));
-    counter = i;
     if(!colors.length) {
         for(var prop in obj) {
             if (obj[prop] instanceof Array) {
@@ -20,17 +18,20 @@ btnColor.addEventListener('click', function () {
             };
         };
     };
-    body.style.backgroundColor = colors[i];
+    
+    counter = Math.floor(Math.random() * (colors.length));
+    body.style.backgroundColor = colors[counter];
 
 });
 
 body.addEventListener('keydown', function (e) {
-    
+    var isLeftKey = e.code === 'ArrowLeft';
+    var isRightKey = e.code === 'ArrowRight';
 
-    if ((e.code === 'ArrowRight' && colors.length === 0) || (e.code === 'ArrowLeft' && colors.length === 0)) {
-        alert ('Загрузите массив');
+    if ((isLeftKey || isRightKey) && !colors.length) {
+        alert ('Загрузите массив, пожалуйста!');
     };
-
+    
     if (e.code === 'ArrowRight' && counter < colors.length - 1){
         body.style.backgroundColor = colors[++counter];
 
@@ -39,14 +40,13 @@ body.addEventListener('keydown', function (e) {
         body.style.backgroundColor = colors[counter];
     };
 
-
     if (e.code === 'ArrowLeft' && counter <= colors.length - 1 && counter > 0) {
         body.style.backgroundColor = colors[--counter];
 
     } else if (e.code === 'ArrowLeft' && counter <= 0 ) {
         counter = colors.length - 1;
         body.style.backgroundColor = colors[counter];
-    }
+    };
 
 });
 
