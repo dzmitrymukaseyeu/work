a6f6f7ad879a1ca188ad0b5a8bc63d46d168b4d7
dzmitrymukaseyeu <dimamukas@gmail.com>
2020-05-21 23:56:17 +0300

    change some if to else if

--- HomeworkGame/js/main.js
+++ HomeworkGame/js/main.js
@@ -3,32 +3,19 @@ var bShooter = document.querySelector('.b-shooter');
 var bShooterAim = document.querySelector('.b-shooter__aim');
 var bShooterImgAim = document.querySelector('.b-shooter__img-aim');
 
-
-
-
 bShooter.addEventListener('click', function (e) {
     var x = e.offsetX - bShooterAim.offsetWidth / 2;
     var y = e.offsetY - bShooterAim.offsetHeight / 2;
     var limitX = bShooter.offsetWidth - bShooterAim.offsetWidth;
     var limitY = bShooter.offsetHeight - bShooterAim.offsetHeight;
 
-    if (e.target.tagName.toLowerCase() === 'img') {
-        return bShooterAim.style.transform ;
-    };
-
     if (x > limitX) {
         x = limitX
-    };
-
-    if ( y > limitY) {
+    } else if (y > limitY) {
         y = limitY
-    };
-
-    if (x < 0) {
+    } else if (x < 0) {
         x = 0;
-    };
-
-    if (y < 0) {
+    } else if (y < 0) {
         y = 0;
     }; 
 
