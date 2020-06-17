e04055c7958746b638c7dfe91582c082c4fd76ee
dzmitrymukaseyeu <dimamukas@gmail.com>
2020-06-17 18:35:44 +0300

    added parallel and sequential request

--- Homework 10/main.js
+++ Homework 10/main.js
@@ -1,41 +1,67 @@
+'use strict';
+
+
 const requestForUser = function(succes, error, url) {
 
     const xhr = new XMLHttpRequest();
 
-    xhr.open('GET', url);
+    xhr.open('GET', "https://randomuser.me/api/?results=3&gender" + url);
 
     xhr.onreadystatechange = function(){
         if (xhr.readyState === 4) {
             if(xhr.status >= 200 && xhr.status < 300) {
                 succes(JSON.parse(xhr.response));
-                ;
             } else {
                 error({
-                    code: 500,
-                    message: 'Server Error'
+                    code: xhr.status,
+                    message: xhr.responseText
                 })
-            }
-        }
-    }
+            };
+        };
+    };
 
     xhr.send();
 };
 
 
-requestForUser(function(male){
-    console.log(male);
-},
-function(error) {
-console.log(error);   
-},
-"https://randomuser.me/api/?results=3&gender=male"
+requestForUser(
+    function(male){
+        console.log(male);
+    },
+    function(error) {
+    console.log(error);   
+    },
+    "=male"
 );
 
-requestForUser(function (female) {
-    console.log(female);
-},
+requestForUser(
+    function (female) {
+        console.log(female);
+    },
     function (error) {
         console.log(error);
     },
-    "https://randomuser.me/api/?results=3&gender=female"
+    "=female"
+);
+
+
+
+requestForUser(
+    function (male) {
+        console.log(male);
+        
+        requestForUser(
+            function (female) {
+                console.log(female);
+            },
+            function (error) {
+                console.log(error);
+            },
+            "=female"
+        );
+    },
+    function (error) {
+        console.log(error);
+    },
+    "=male"
 );
