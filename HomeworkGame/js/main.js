64d25f0b9b0c36dee55be37f1c71d7b37929c5ac
dzmitrymukaseyeu <dimamukas@gmail.com>
2020-06-23 00:24:27 +0300

    added animation for ghost. Added health and progress bar

--- HomeworkGame/js/main.js
+++ HomeworkGame/js/main.js
@@ -7,6 +7,10 @@ var bShooterImgFire = document.querySelector('.b-shooter__img-fire');
 var delayToReset = 500;
 
 bShooter.addEventListener('click', function (e) {
+    if (ghost.style.animationPlayState === 'paused') {
+        return;
+    }
+
     var x = e.offsetX - bShooterAim.offsetWidth / 2;
     var y = e.offsetY - bShooterAim.offsetHeight / 2;
     var limitX = bShooter.offsetWidth - bShooterAim.offsetWidth;
@@ -45,7 +49,7 @@ body.addEventListener('keyup', function (e) {
         if (
             aimCenterX <= coordGhost.right - 20
             && aimCenterX >= coordGhost.left + 20
-            && aimCenterY >= coordGhost.top -20
+            && aimCenterY >= coordGhost.top - 20
             && aimCenterY <= coordGhost.bottom + 20
         ) {
             var animationProperties = `opacity: 0;
@@ -56,6 +60,8 @@ body.addEventListener('keyup', function (e) {
             bShooterImgFire.style.cssText += animationProperties;
             ghost.style.cssText += animationProperties;
             bShooterImgAim.style.display = 'none';
+            ghost.style.animationPlayState = 'paused';
+
 
             setTimeout(function() {
                 bShooterImgFire.removeAttribute('style');
@@ -69,35 +75,25 @@ body.addEventListener('keyup', function (e) {
 });
 
 function setRandomCoords () {
-    var x = Math.floor(Math.random() * (bShooter.offsetWidth - ghost.offsetWidth / 2));
-    var y = Math.floor(Math.random() * (bShooter.offsetHeight - ghost.offsetHeight / 2));
     var limitX = bShooter.offsetWidth - ghost.offsetWidth;
     var limitY = bShooter.offsetHeight - ghost.offsetHeight;
 
-    if (x > limitX) {
-        x = limitX;
-    } else if (x < 0) {
-        x = 0;
-    };
-
-    if (y > limitY) {
-        y = limitY;
-    } else if (y < 0) {
-        y = 0;
-    }; 
+    var x = Math.floor(Math.random() * (limitX + 1));
+    var y = Math.floor(Math.random() * (limitY + 1));
 
     ghost.style.left = x + 'px';
     ghost.style.top = y + 'px';
 };
 
 setInterval(function() {
-    if (ghost.style.transitionDelay) {
-        return;
-    };
 
     if (ghost.style.display === 'none') {
         ghost.style.display = '';
     };
+
+    if (ghost.style.animationPlayState ==='paused') {
+        return;
+    }
 
     setRandomCoords();
 }, 3000);
