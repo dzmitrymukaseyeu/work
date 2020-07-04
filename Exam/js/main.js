7b144feefbc2deacdf34dcde0a8d22ed295a6cd8
dzmitrymukaseyeu <dimamukas@gmail.com>
2020-07-04 23:10:42 +0300

    added PUSH request

--- Exam/js/main.js
+++ Exam/js/main.js
@@ -1,21 +1,31 @@
 "use strict";
 
-fetch('https://randomuser.me/api/?results=10')
-.then(
-    function (res) {
-        return res.json();
-    }
-).then(
-    function ({ results }) {
-        addUsers(results);
-        removeUser();
-    }
-).catch(
-    function (err) {
-        console.log(err);
-    }
-)
 
+function getUsers() {
+
+    document.body.classList.toggle('_loading');
+
+
+    fetch('https://randomuser.me/api/?results=10')
+    .then(
+        function (res) {
+            return res.json();
+        }
+    )
+    .then(
+        function ({ results }) {
+            console.log(results);
+            addUsers(results);
+            removeUser();
+        }
+    )
+    .catch(
+        function (err) {
+            console.log(err);
+        }
+    )
+    .finally(() => document.body.classList.toggle('_loading'));
+}
 
 
 
@@ -34,7 +44,7 @@ function addUsers(arg) {
                 <div class="user__inner__info-img ${item.gender === "female" ? "_female" : ''}">
                     <img src="${item.picture.large}" alt="user-img">
                 </div>
-                <button class="user__inner__info-button" type="button"></button>
+                <button class="user__inner__info-button" type="button" data-id='${item.login.uuid}'></button>
                 <div class="user__inner__info-name">
                     <h3>${item.name.title} ${item.name.first} ${item.name.last}</h3>
                 </div>
@@ -47,8 +57,6 @@ function addUsers(arg) {
         })
 
         conteiner.appendChild(ul);
-        document.getElementsByClassName('preloader')[0].remove();
-
 };
 
 function removeUser() {
@@ -56,7 +64,37 @@ function removeUser() {
 
     ul.addEventListener('click', function (e) {
         if (e.target.tagName.toLowerCase() === 'button') {
-            e.target.parentNode.remove(e);
+            deleteUser(e.target);
         }
     })
 }
+
+
+function deleteUser (element) {
+
+    const id = element.dataset.id;
+
+    document.body.classList.toggle('_loading');
+
+    fetch('https://httpstat.us/200', 
+    {
+        method: 'POST',
+        body: JSON.stringify({ id })
+    })
+    .then(res => {
+        console.log(res);
+
+        if(res.ok) {
+            element.parentNode.remove()
+        }
+    })
+    .catch(
+        function (err) {
+            console.log(err);
+        }
+    )      
+    .finally(() => document.body.classList.toggle('_loading'))
+}
+
+
+getUsers();
